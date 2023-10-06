4a7469ab50abeb39883309e274ea0cb4baf5f583
Kevin Yobeth <leokeviny@gmail.com>
2023-10-06 22:23:00 +0700

    Add handler

--- model/order.go
+++ model/order.go
@@ -7,16 +7,16 @@ import (
 )
 
 type Order struct {
-	OrderId    uint64     `json:order_id`
-	CustomerId uuid.UUID  `json:customer_id`
-	LineItems  []LineItem `json:line_items`
-	CreatedAt  *time.Time `json:created_at`
-	ShippedAt  *time.Time `json:shipped_at`
-	CompltedAt *time.Time `json:completed_at`
+	OrderId     uint64     `json:"order_id"`
+	CustomerId  uuid.UUID  `json:"customer_id"`
+	LineItems   []LineItem `json:"line_items"`
+	CreatedAt   *time.Time `json:"created_at"`
+	ShippedAt   *time.Time `json:"shipped_at"`
+	CompletedAt *time.Time `json:"completed_at"`
 }
 
 type LineItem struct {
-	ItemId   uuid.UUID `json:item_id`
-	Quantity uint      `json:quantity`
-	Price    uint      `json:price`
+	ItemId   uuid.UUID `json:"item_id"`
+	Quantity uint      `json:"quantity"`
+	Price    uint      `json:"price"`
 }
