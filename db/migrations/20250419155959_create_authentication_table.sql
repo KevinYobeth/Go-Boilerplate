0e719885d91b4c43499be5bcbfa8ce4677a20518
Kevin Yobeth <leokeviny@gmail.com>
2025-06-13 10:16:05 +0700

    chore: remove unused migrations

--- db/migrations/20250419155959_create_authentication_table.sql
+++ db/migrations/20250419155959_create_authentication_table.sql
@@ -14,29 +14,12 @@ CREATE TABLE users (
   deleted_by VARCHAR(50)
 );
 
-CREATE TABLE verification_tokens (
-  id UUID NOT NULL PRIMARY KEY,
-  user_id UUID NOT NULL REFERENCES users(id),
-  token VARCHAR(255) NOT NULL,
-  used_at TIMESTAMP,
-  expires_at TIMESTAMP NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  created_by VARCHAR(50) NOT NULL,
-  updated_at TIMESTAMP,
-  updated_by VARCHAR(50),
-  deleted_at TIMESTAMP,
-  deleted_by VARCHAR(50)
-);
-
 CREATE INDEX idx_users_email ON users(email);
-CREATE INDEX idx_verification_tokens_user_id ON verification_tokens(user_id);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS verification_tokens;
 DROP TABLE IF EXISTS users;
 
 DROP INDEX IF EXISTS idx_users_email;
-DROP INDEX IF EXISTS idx_verification_tokens_user_id;
 -- +goose StatementEnd
